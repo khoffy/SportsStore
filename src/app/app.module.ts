7bb4149bb908394085e6e78c8e44dec41fed7df0
khoffy <adolf_hermann86@yahoo.fr>
2023-09-11 16:18:12 -0500

    Creating module and component manually and setting up root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        StoreModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/store/store.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { ProductRepository } from "../model/product.repository";
+import { Product } from "../model/product.model";
+
+@Component({
+    selector: "store",
+    templateUrl: "./store.component.html"
+})
+export class StoreComponent {
+
+    public selectedCategory: string = null!;
+
+    constructor(private repository: ProductRepository) {}
+
+    get products(): Product[] {
+        return this.repository.getProducts(this.selectedCategory);
+    }
+
+    get categories(): string[] {
+        return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory as string;
+    }
+}
+
