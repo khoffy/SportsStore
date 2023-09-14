fcb2b14818674f1a27b9cda02f3b24e636cf383f
khoffy <adolf_hermann86@yahoo.fr>
2023-09-14 14:58:15 -0500

    Creating card - Updating some related code

---
+++ src/app/model/cart.model.ts
@@ -0,0 +1,65 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./product.model";
+
+@Injectable()
+export class Cart {
+    public lines: CartLine[] = [];
+    public itemCount = 0;
+    public cartPrice = 0;
+
+    addLine(product: Product, quantity = 1) {
+        const line = this.lines.find(line => line.product.id == product.id);
+        if (line != undefined) {
+            line.quantity = Number(quantity);
+        } else {
+            this.lines.push(new CartLine(product, quantity));
+        }
+        this.recalculate();
+    }
+
+    updateQuantity(product: Product, quantity: number) {
+        const line = this.lines.find(line => line.product.id == product.id);
+        if (line != undefined) {
+            line.quantity = Number(quantity);
+        }
+        this.recalculate();
+    }
+
+    removeLine(id: number) {
+        const index = this.lines.findIndex(line => line.product.id == id);
+        this.lines.splice(index, 1);
+        this.recalculate();
+    }
+
+    clear() {
+        this.lines = [];
+        this.itemCount = 0;
+        this.cartPrice = 0;
+    }
+
+    private recalculate() {
+        this.itemCount = 0;
+        this.cartPrice = 0;
+        this.lines.forEach(l => {
+            this.itemCount += l.quantity;
+            if (l.product.price != undefined) {
+                this.cartPrice += (l.quantity * l.product.price)
+            }
+        });
+    }
+}
+
+export class CartLine {
+    result = 0;
+    constructor(public product: Product, public quantity: number) {
+
+    }
+
+    get lineTotal() {
+        if (this.product.price != undefined) {
+            this.result = this.quantity * this.product.price;
+        }
+        return this.result;
+    }
+}
+
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
+import { Cart } from "../model/cart.model";
 
 @Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
     selector: "store",
     templateUrl: "./store.component.html"
 })
@@ -12,10 +14,11 @@ export class StoreComponent {
     public productsPerPage = 4;
     public selectedPage = 1;
 
-    constructor(private repository: ProductRepository) {}
+    constructor(private repository: ProductRepository,
+        private cart: Cart) {}
 
     get products(): Product[] {
-        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
         return this.repository.getProducts(this.selectedCategory)
             .slice(pageIndex, pageIndex + this.productsPerPage);
     }
@@ -40,6 +43,10 @@ export class StoreComponent {
     get pageCount(): number {
         return Math.ceil(this.repository
             .getProducts(this.selectedCategory).length/this.productsPerPage)
+    }
+
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
 
     // get pageNumbers(): number[] {
